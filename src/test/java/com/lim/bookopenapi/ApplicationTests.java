4dab00a5e156da3bb4101b1e8f61231193a24ae6
saemyunglim <saemyung2000@naver.com>
2020-07-05 15:57:39 +0900

    Initial commit

---
+++ src/test/java/com/lim/bookopenapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lim.bookopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
